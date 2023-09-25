7d8288649c4a4958d797313c0f4c3a87a289cf85
Daiana Belen Garcia <103230866+daianabelengarcia@users.noreply.github.com>
2023-09-25 19:31:32 -0300

    sprites-raulcito

--- TpFinal/Assets/Scripts/PlayerController.cs
+++ TpFinal/Assets/Scripts/PlayerController.cs
@@ -13,43 +13,55 @@ public class PlayerController : MonoBehaviour
     public EnemiShoot enemigo;
 
     private Rigidbody2D rb;
-    private GroundDetector groundDetector; // Referencia al detector de suelo.
 
     private int colisiones = 0; // Contador de colisiones.
     public int vidas = 3; // Número máximo de colisiones antes de perder.
 
+    private Animator Animator;
+
+    public bool isGrounded = false;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        groundDetector = GetComponent<GroundDetector>(); // Obtén la referencia al detector de suelo.
 
         villano_pos = GameObject.Find("Villain").transform; //Villano
         enemigo = villano_pos.GetComponent<EnemiShoot>();
 
+        Animator = GetComponent<Animator>();
     }
 
     private void Update()
     {
         // Mover a la izquierda
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            rb.velocity = new Vector2(-moveSpeed, rb.velocity.y);
-        }
-        // Mover a la derecha
-        else if (Input.GetKey(KeyCode.RightArrow))
-        {
-            rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
-        }
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                rb.velocity = new Vector2(-moveSpeed, rb.velocity.y);
+                transform.localScale = new Vector3(-2, 2, 2);
+                Animator.SetBool("Camina", true);
+            }
+            // Mover a la derecha
+            else if (Input.GetKey(KeyCode.RightArrow))
+            {
+                rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+                transform.localScale = new Vector3(2, 2, 2);
+                Animator.SetBool("Camina", true);
+            }
         else
         {
             rb.velocity = new Vector2(0, rb.velocity.y);
+            Animator.SetBool("Camina", false);
         }
-
         // Saltar solo si está en el suelo
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-        }
+            if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+                    Animator.SetBool("Salta", true);
+                }
+            else
+            {
+                Animator.SetBool("Salta", false);
+            }
 
         if (villano_pos.position.x > this.transform.position.x)
         {
@@ -70,6 +82,11 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    //public bool IsGrounded()
+    //{
+    //    return isGrounded;
+    //}
+
     void OnCollisionEnter2D(Collision2D colision)
     {
         if (colision.gameObject.CompareTag("Bala"))
@@ -78,10 +95,19 @@ public class PlayerController : MonoBehaviour
 
             if (colisiones >= vidas)
             {
-                // Si el contador alcanza el máximo, llama a la función de muerte.
-                Die();
+                //Si el contador alcanza el máximo, llama a la función de muerte.
+               Die();
             }
         }
+
+        //if (colision.gameObject.CompareTag("Ground")) // Asegúrate de que el tag del suelo sea "Ground".
+        //{
+        //    isGrounded = true;
+        //}
+        //else
+        //{
+        //    isGrounded = false;
+        //}
     }
 
     void Die()
